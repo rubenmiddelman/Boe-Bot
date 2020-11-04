9a2c3038fbc615e02fa30101f169ff3fb56101c6
Ruben Middelman <rubenmiddelman@gmail.com>
2020-11-04 23:58:10 +0100

    adds a almoste final version of the boebot project

--- boebot.ino
+++ boebot.ino
@@ -1,46 +1,104 @@
+/*//////////////////////////////////////////////////
+//  Project Padvolginator                         //
+//Door: Stephan, Skip, Ruben en Tristan           //
+//Datum: 04/01/2020                               //
+// Stephan strengers: 1786764                     //
+// Skip Wijtman: 1787390                          //
+// Tristan de gans: 1786830                       //
+// Ruben Middelman: 1740398                       //
+*///////////////////////////////////////////////////
+
 #include <Servo.h>
 
-Servo servoL; // voegt servo Links toe.
-Servo servoR; // voegt servo Rechts toe.
+  //Prototypes
+  void setup();
+  void loop();
+  int Afstand_Sensor();
+  void SensorWaardeLezen();
+  void CasePicker(int WelkeCase);
+  void CaseFinder();
+  void Rechtdoor();
+  void BochtNaarLinks(int niveauVanBocht, int tegenDraai);
+  void BochtNaarRechts(int niveauVanBocht, int tegenDraai);
+  void AchterUit();
+  
 
-int sensor1 = A0;
-int sensor2 = A1;
-int sensor3 = A2;
+Servo servoL; //Voegt servo Links toe
+Servo servoR; //Voegt servo Rechts toe
+Servo arm;    //Voegt servo arm toe
+
+//Alle globale variabelen
+int sensor1 = A1;            //Variabelen voor de sensoren
+int sensor2 = A2;   
+int sensor3 = A0;
 int sensor4 = A3;
-int sensorWaarde1 = 0;
+int sensorWaarde1 = 0;       //Variabelen voor de sensorwaardes
 int sensorWaarde2 = 0;
 int sensorWaarde3 = 0;
 int sensorWaarde4 = 0;
-float vertrager = 0.1;
-int rechtsOm = 1400;
-int linksOm = 1600;
-int caseNumber;
-int threshold = 264;
-int LED1 = 4;
+int rechtsOm = 1400;         //Servo draait tegen de klok in
+int linksOm = 1600;          //Servo draait met de klok mee
+int caseNumber;              //Variabele om de cases te zoeken
+int threshold = 650;         //Threshold voor de sensor
+int LED1 = 4;                //Welke led aan welke pin
 int LED2 = 2;
-int LED3 = 3;
-int LED4 = 5;
-int bochtVal;
+int LED3 = 5;
+int LED4 = 3;
+int bochtVal;                //Variabele die de snelheid van de servo bepaalt 
+int Sharp_sensor = A5;       //Sharp sensor welke pin
+int Sharp_sensor_ledje = 8;  //Sharp sensor Ledje welke pin
+int Sharp_sensor_waarde;     //variabele voor de waardes die worden uitgelezen door de sharp sensor
 
 void setup() {
   // stelt de baudrate in zodat de seriele monitor werkt
   Serial.begin(9600);
-  pinMode(LED1, OUTPUT);
+  pinMode(LED1, OUTPUT);     //Output voor de ledjes
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
   pinMode(LED4, OUTPUT);
   servoL.attach(11);    //koppelt servoL aan pin 11
   servoR.attach(10);    //koppelt servoR aan pin 10
+  arm.attach(12);       //koppelt de servo arm aan pin 12 
 }
 
+//Door: Ruben en Tristan
+//De functies in de main loop
 void loop() {
   SensorWaardeLezen();
   CasePicker(caseNumber);
   CaseFinder();
+  Afstand_Sensor();
 }
 
+//Door: Stephan en Skip
+//Deze functie zorgt ervoor dat een object wordt gedetecteerd en dat de servo arm gaat draaien
+int Afstand_Sensor()
+{
+  Sharp_sensor_waarde = analogRead(Sharp_sensor);
+  Serial.println(Sharp_sensor_waarde);
 
-//leest alle sensorwaardes op en zet ze in een ander variable
+  if (Sharp_sensor_waarde > 400)
+  {
+    digitalWrite(Sharp_sensor_ledje, HIGH);
+    Serial.println("STOP!!");
+    servoL.writeMicroseconds(1500);
+    servoR.writeMicroseconds(1500);
+
+    arm.writeMicroseconds(2500);
+    delay(6000);
+    arm.writeMicroseconds(0);
+
+    delay(2000);
+  }
+  else
+  {
+    digitalWrite(Sharp_sensor_ledje, LOW);
+    delay(100);
+  }
+}
+
+//Door Stephan
+//Deze functie leest alle sensorwaardes op en zet ze in een ander variabele
 void SensorWaardeLezen() {
   sensorWaarde1 = analogRead(sensor1);
   sensorWaarde2 = analogRead(sensor2);
@@ -48,123 +106,190 @@ void SensorWaardeLezen() {
   sensorWaarde4 = analogRead(sensor4);
 }
 
-
+//Door Ruben, Tristan, Stephan en Skip
+//Deze functie kijkt naar de waardes die uit de sensoren komen en kiest daarbij de bijbehorende case
+//Deze case stuurt de robot
 void CasePicker(int welkeCase) {
+  int i;
+  
   switch (welkeCase) {
-    //Case 0 is als geen een sensor iets uitlaat
+    //Sensor 0
     case 0:
-      AchterUit();
       Serial.println("Padvolger is van de baan af zet hem terug");
-
       digitalWrite(LED1, LOW);
       digitalWrite(LED4, LOW);
       digitalWrite(LED2, LOW);
       digitalWrite(LED3, LOW);
       // zorg er voor dat alle ledjes beginnen te knipper
       break;
-    //Case 1 is als sensor 1 hoog uitslaat
+    //Sensor 1
     case 1:
-      // zorg dat de robot recht door blijft gaan
+      Rechtdoor();
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
       Serial.println("case1");
-      BochtNaarLinks(-30, 30);
       break;
 
-    //case 2 is als sensor 2 hoog uitslaat
+    //Sensor 2
     case 2:
-      //zorg dat boebot scherp naar links gaat
+      BochtNaarLinks(-20, 100);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED3, LOW);
       Serial.println("case2");
-      BochtNaarLinks(-20, 40);
-      break;
-
-    //sensor 3
-    case 3:
-      //bocht naar rechts
-      Serial.println("case3");
-      BochtNaarRechts(30, -20);
-      break;
-
-    //sensor 4
-    case 4:
-      // boebot gaat zeer scherp naar rechts
-      Serial.println("case4");
-      BochtNaarRechts(-10, -30);
       break;
 
     //sensor 1 en 2
-    case 5:
-      // maak een bocht naar links
-      Serial.println("case5");
-      BochtNaarRechts(-20, -40);
+    case 3:
+      BochtNaarRechts(-20, -80);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      Serial.println("case3");
+      break;
+
+    //sensor 3
+    case 4:
+      Rechtdoor();
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED4, LOW);
+      digitalWrite(LED2, LOW);
+      Serial.println("case4");
       break;
 
     //sensor 1 en 3
-    case 6:
-      // boebot gaat rechtdoor
-      Serial.println("case6");
+    case 5:
       Rechtdoor();
-      break;
-
-    //sensor 1 en 4
-    case 7:
-      //slappe bocht naar recht
-      Serial.println("case7");
-      BochtNaarLinks(-20, 40);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED3, LOW);
+      Serial.println("case5");
       break;
 
     //sensor 2 en 3
+    case 6:
+
+      if ((welkeCase != 10) || (welkeCase != 11) || (welkeCase != 14) || (welkeCase != 15))
+      {
+      for (i = 0; i < 1000000; i++) {
+        SensorWaardeLezen();
+        BochtNaarLinks(-20, 100);
+        digitalWrite(LED2, HIGH);
+        digitalWrite(LED3, HIGH);
+        digitalWrite(LED1, LOW);
+        digitalWrite(LED4, LOW);
+        Serial.println("case6");
+        if (sensorWaarde1  > threshold ) {
+          break;
+        }
+      }
+      }
+      break;
+    //sensor 1, 2 en 3
+    case 7:
+      Rechtdoor();
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, LOW);
+      Serial.println("case7");
+      break;
+
+    //sensor 4
     case 8:
-      //heel erg scherp naar links
+      BochtNaarRechts(100, -100);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
       Serial.println("case8");
-      BochtNaarLinks(10, 30);
+      break;
+
+    //sensor 1 en 4
+    case 9:
+      BochtNaarLinks(20, 80);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      Serial.println("case9");
       break;
 
     //sensor 2 en 4
-    case 9:
-      // op hoop van zegen de boebot 90 graden draaien
-      Serial.println("case9");
-      BochtNaarRechts(40, -40);
-      break;
-
-    //sensor 3 en 4
     case 10:
-      //zeer scherpe bocht naar rechts
-      Serial.println("case10");
-      BochtNaarRechts(10, -40);
-      break;
-
-    //sensor 1, 2, 3
-    case 11:
-      // boebot moet rechtdoor
-      Serial.println("case11");
       Rechtdoor();
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED3, LOW);
+      Serial.println("case10");
       break;
 
-    //sensor 2, 3, 4
+    //sensor 1, 2, 4
+    case 11:
+      Rechtdoor();
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED3, LOW);
+      Serial.println("case11");
+      break;
+
+    //sensor  3, 4
     case 12:
-      //op hoop van zegen boebot 90 grade draaien
-      Serial.println("case12");
-      BochtNaarRechts(40, -40);
+      if ((welkeCase != 10) || (welkeCase != 11) || (welkeCase != 14) || (welkeCase != 15))
+      {
+      for (i = 0; i < 1000000; i++) {
+        SensorWaardeLezen();
+        BochtNaarRechts(20, -100);
+        digitalWrite(LED3, HIGH);
+        digitalWrite(LED4, HIGH);
+        digitalWrite(LED1, LOW);
+        digitalWrite(LED2, LOW);
+        Serial.println("case12");
+        if (sensorWaarde1  > threshold) {
+          break;
+        }
+      }
+      }
       break;
 
     //sensor 1, 3, 4
     case 13:
-      //boebot moet rechtdoor
-      Serial.println("case13");
       Rechtdoor();
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED2, LOW);
+      Serial.println("case13");
+
+      //Rechtdoor();
       break;
 
-    //sensor 1, 2, 4
+    //sensor  2, 3, 4
     case 14:
-      //boebot gaat rechtdoor
-      Serial.println("case14");
       Rechtdoor();
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      digitalWrite(LED1, LOW);
+      Serial.println("case14");
       break;
 
     //sensor 1, 2, 3, 4
     case 15:
-      Serial.println("case15");
-      //boebot gaat rechtdoor
       Rechtdoor();
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      Serial.println("case15");
       break;
 
     //is onnodig maar laten we het toch maar doen
@@ -175,120 +300,46 @@ void CasePicker(int welkeCase) {
   }
 }
 
+//Door Stephan
+//Dit is onze manier om onze cases te vinden door middel van de waardes die we binnen krijgen van de sensoren. Zie Werking Software van het verslag
 void CaseFinder() {
-  //just a bunch of if statements that find which case should be used
-  if (sensorWaarde1 > 100) {
-    caseNumber = 1;
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED4, LOW);
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED3, LOW);
-    if (sensorWaarde2 > threshold) {
-      caseNumber = 5;
-      digitalWrite(LED1, HIGH);
-      digitalWrite(LED4, LOW);
-      digitalWrite(LED2, HIGH);
-      digitalWrite(LED3, LOW);
-      if (sensorWaarde3 > threshold) {
-        caseNumber = 11;
-        digitalWrite(LED1, HIGH);
-        digitalWrite(LED4, HIGH);
-        digitalWrite(LED2, HIGH);
-        digitalWrite(LED3, LOW);
-        if (sensorWaarde4 > threshold) {
-          caseNumber = 15;
-          digitalWrite(LED1, HIGH);
-          digitalWrite(LED4, HIGH);
-          digitalWrite(LED2, HIGH);
-          digitalWrite(LED3, HIGH);
-        }
-      } else if (sensorWaarde4 > threshold) {
-        caseNumber = 14;
-      }
-    }
-    if (sensorWaarde3 > threshold) {
-      digitalWrite(LED1, HIGH);
-      digitalWrite(LED4, HIGH);
-      digitalWrite(LED2, LOW);
-      digitalWrite(LED3, LOW);
-      caseNumber = 6;
-      if (sensorWaarde4 > threshold) {
-        caseNumber = 13;
-      }
-    }
-    if (sensorWaarde4 > threshold) {
-      caseNumber = 7;
-    }
-  } else if (sensorWaarde2 > threshold) {
-    caseNumber = 2;
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED4, LOW);
-    digitalWrite(LED3, LOW);
-    if (sensorWaarde3 > threshold) {
-      caseNumber = 8;
-      digitalWrite(LED1, LOW);
-      digitalWrite(LED4, HIGH);
-      digitalWrite(LED2, HIGH);
-      digitalWrite(LED3, LOW);
-      if (sensorWaarde4 > threshold) {
-        caseNumber = 12;
-      }
-    }
-    if (sensorWaarde4 > threshold) {
-      caseNumber = 9;
-    }
-  } else if (sensorWaarde3 > threshold) {
-    caseNumber = 3;
-    digitalWrite(LED3, LOW);
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED4, HIGH);
-    if (sensorWaarde4 > threshold) {
-      caseNumber = 10;
-      digitalWrite(LED1, LOW);
-      digitalWrite(LED4, HIGH);
-      digitalWrite(LED2, LOW);
-      digitalWrite(LED3, HIGH);
-    }
-  } else if (sensorWaarde4 > threshold) {
-    caseNumber = 4;
-    digitalWrite(LED4, LOW);
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED3, HIGH);
-  } else {
-    caseNumber = 0;
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED4, LOW);
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED3, LOW);
-  }
-
+  caseNumber =
+    (sensorWaarde1 > threshold) * 1 +
+    (sensorWaarde2 > threshold) * 2 +
+    (sensorWaarde3 > threshold) * 4 +
+    (sensorWaarde4 > threshold) * 8;
 }
 
+//Door Ruben en Tristan
+//Functie om de Boe-bot rechtdoor te laten rijden
 void Rechtdoor() {
-  int a;
-  for (a = 0; a < 8; a++) {
-    servoL.writeMicroseconds(rechtsOm);
-    servoR.writeMicroseconds(linksOm);
+  int i;
+  for (i = 0; i < 128; i++) {
+    servoL.writeMicroseconds(1430);
+    servoR.writeMicroseconds(1570);
   }
 }
 
+//Door Ruben en Tristan
+//Functie om de Boe-bot een bocht naar links te laten maken
 void BochtNaarLinks(int niveauVanBocht, int tegenDraai) {
   servoL.writeMicroseconds(1500 - tegenDraai);
   servoR.writeMicroseconds(1500 + niveauVanBocht);
 }
 
+//Door Ruben en Tristan
+//Functie om de Boe-bot een bocht naar rechts te laten maken
 void BochtNaarRechts(int niveauVanBocht, int tegenDraai) {
   servoL.writeMicroseconds(1500 + niveauVanBocht);
   servoR.writeMicroseconds(1500 - tegenDraai);
 }
 
+//Door Ruben en Tristan
+//Functie om de Boe-bot rechtdoor te laten rijden
 void AchterUit() {
   volatile int i;
   for (i = 0; i < 1; i++) {
-    servoL.writeMicroseconds(linksOm);
-    servoR.writeMicroseconds(rechtsOm);
+    servoL.writeMicroseconds(1550);
+    servoR.writeMicroseconds(1450);
   }
 }
